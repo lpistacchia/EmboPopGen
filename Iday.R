6a342f1819252c44767f8e100b51b1c7bdfa2077
lpistacchia <letizia.pistacchia@uniroma1.it>
2024-06-11 16:46:47 +0200

    edit

--- Iday.R
+++ Iday.R
@@ -1,7 +1,3 @@
 #EMBO_IDAY 
 #library(usethis)
 
-usethis::use_git_config(user.name = "lpistacchia", user.email = "letizia.pistacchia@uniroma1.it")
-usethis::git_sitrep()
-
-usethis::create_github_token()
